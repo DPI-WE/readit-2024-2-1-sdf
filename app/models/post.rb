557be31be9627a0e2d869666df62f90bfc55e4cb
Ian Heraty <heratyian@gmail.com>
2023-11-10 08:41:54 -0600

    Add ransack search/sort for posts#index

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,12 @@
 class Post < ApplicationRecord
   belongs_to :user, required: true, class_name: "User", foreign_key: "user_id"
   has_many :comments, class_name: "Comment", foreign_key: "post_id", dependent: :destroy
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["content"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["user"]
+  end
 end
