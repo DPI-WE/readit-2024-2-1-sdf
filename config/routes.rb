f32c5ef70c6ca2d14207f1097fab45e544f26eca
Ian Heraty <heratyian@gmail.com>
2023-12-04 14:28:44 -0600

    Add ahoy and blazer for analytics

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   authenticate :user, ->(user) { user.admin? } do
+    mount Blazer::Engine, at: "blazer"
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   end
 
