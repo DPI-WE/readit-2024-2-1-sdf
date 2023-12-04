9f5c39d77e50ec4e14fffca5fa79462c21109c1e
Ian Heraty <heratyian@gmail.com>
2023-12-04 13:42:45 -0600

    Add rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  authenticate :user, ->(user) { user.admin? } do
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  end
+
   devise_for :users
 
   resources :comments
